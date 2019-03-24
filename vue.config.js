dabc26c9c16e4b98c51cb4e7e0b9e6a63b8b7e30
html546 <1776787598@qq.com>
2019-03-24 12:59:09 +0800

    添加注释

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const resolve = dir => path.join(__dirname, dir);
-
+// 判断环境是生产环境还是打包环境
 const BASE_URL = process.env.NODE_ENV === 'production' ? '/iveiw-admin' : '/'
 module.exports = {
   lintOnSave: false,
@@ -14,6 +14,7 @@ module.exports = {
   },
   // 打包时不生成.map文件
   productionSourceMap: false,
+  // 跨域配置
   devServer: {
     proxy: 'http://localhost:4000'
   }
